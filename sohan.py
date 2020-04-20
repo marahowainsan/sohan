682cc25c7ae0adcd6f5c53cabcb0bc476417d820
marahowainsan <63833817+marahowainsan@users.noreply.github.com>
2020-04-20 18:45:11 +0500

    Update sohan.py

--- sohan.py
+++ sohan.py
@@ -47,19 +47,12 @@ def jalan(z):
 
 
 ##### LOGO #####
-logo = """ -----------------------------•◈•
-(  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
-|__~~~\ •◈•-----█-------⑦-------█------•◈•
-|__~~~\ •◈•-----█-------⑧-------█------•◈•
-|__~~~\ •◈•-----█-------⑥-------█------•◈•
+🅂🄾🄷🄰🄽◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mmarahowainsan
+\033[1;96mInstagram \033[1;93m: \033[1;Flowkashif
+\033[1;96mFacebook  \033[1;93m: \033[1; kashibaloch78
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/marahowainsan/sohan
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +73,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mmarahowainsan
+\033[1;96mInstagram \033[1;93m: \033[1;92mkashif
+\033[1;96mFacebook  \033[1;93m: \033[1;92m kashibaloch78
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/marahowainsan/sohan
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "sohan"
+CorrectPassword = "sohan"
 
 loop = 'true'
 while (loop == 'true'):
